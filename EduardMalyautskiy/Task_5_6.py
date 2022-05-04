5bb6d128d9f6a2f45b88aa0902f365cc7059b782
Edurd M <eduard353@gmail.com>
2022-05-04 09:41:01 +0600

    Modify Task 5.6

---
+++ EduardMalyautskiy/Task_5_6.py
@@ -0,0 +1,24 @@
+def call_once(func):
+    cache = None
+
+    def wrapper(*args):
+        nonlocal cache
+        if cache is None:
+            cache = func(*args)
+        return cache
+
+    return wrapper
+
+
+@call_once
+def sum_of_numbers(a, b):
+    return a + b
+
+
+print(sum_of_numbers(13, 42))
+
+print(sum_of_numbers(999, 100))
+
+print(sum_of_numbers(134, 412))
+
+print(sum_of_numbers(856, 232))
