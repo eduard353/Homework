91522f247bb97f67637818faf7916bb84b82e48e
Eduard Malyautskiy <eduard353@gmail.com>
2022-05-10 16:46:18 +0600

    Add Task_6_4

---
+++ EduardMalyautskiy/Task_6_4.py
@@ -0,0 +1,51 @@
+class Bird:
+
+    def __init__(self, name):
+        self.name = name
+
+    def fly(self):
+        return (f'{self.name} bird can fly')
+
+    def walk(self):
+        return (f'{self.name} bird can walk')
+
+
+class FlyingBird(Bird):
+
+    def __init__(self, name, ration='grains'):
+        super().__init__(name)
+        self.ration = ration
+
+    def __str__(self):
+        return (f'{self.name} bird can walk and fly')
+
+    def eat(self):
+        return (f'It eats mostly {self.ration}')
+
+
+class NonFlyingBird(Bird):
+
+    def __init__(self, name, ration='fish'):
+        super().__init__(name)
+        self.ration = ration
+
+    def fly(self):
+        try:
+            raise AttributeError(f"'{self.name}' object has no attribute 'fly'")
+        except AttributeError:
+            print(f"AttributeError: '{self.name}' object has no attribute 'fly'")
+
+    def swim(self):
+        return (f'{self.name} bird can swim')
+
+    def eat(self):
+        return (f'It eats mostly {self.ration}')
+
+
+class SuperBird(NonFlyingBird, FlyingBird):
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def __str__(self):
+        return (f'{self.name} bird can walk, swim and fly')
