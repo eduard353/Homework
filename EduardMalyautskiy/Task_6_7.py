c161a503a8de0896a6093eb97ce1f470365a2b73
Edurd M <eduard353@gmail.com>
2022-05-11 11:41:25 +0600

    Add Task 6.7

--- EduardMalyautskiy/Task_6_7.py
+++ EduardMalyautskiy/Task_6_7.py
@@ -1,50 +1,50 @@
 from functools import total_ordering
 
-# @total_ordering
+
+@total_ordering
 class Money:
+    exchange_rate = {"EUR": 0.93,
+                     "BYN": 2.1,
+                     "USD": 1}
+
     def __init__(self, value, currency="USD"):
-        self.exchange_rate = {"EUR": 0.93,
-                              "BYN": 2.1,
-                              "USD": 1}
         self.value = value
         self.currency = currency
-        self.usd_value = self.value * self.exchange_rate[self.currency]
 
+    def __str__(self):
+        return str(self.value) + ' ' + self.currency
 
     def convert_to_def(self):
+        return self.value * Money.exchange_rate[self.currency]
 
     def __add__(self, other):
+        if isinstance(other, int):
+            return Money(round(self.value + other / Money.exchange_rate[self.currency], 2), self.currency)
 
-            return str(round((self.usd_value + other.usd_value)/self.exchange_rate[self.currency], 2))+ ' ' + self.currency
+        return Money(round(self.value + other.convert_to_def() / Money.exchange_rate[self.currency], 2), self.currency)
 
+    __radd__ = __add__
 
-    # __radd__ = __add__
-    #
-    # def __mul__(self, other):
-    #     self.usd_value * other
-    #     return self
-    #
-    # __rmul__ = __mul__
-    #
-    # def __sub__(self, other):
-    #     try:
-    #         return self.value - other.value
-    #     except:
-    #         return self.value - other
-    # __rsub__ = __sub__
-    #
-    # def __truediv__(self, other):
-    #     try:
-    #         return self.value / other.value
-    #     except:
-    #         return self.value / other
-    # __rtruediv__ = __truediv__
+    def __mul__(self, other):
+        return Money(self.value * other, self.currency)
 
+    __rmul__ = __mul__
 
-x = Money(10, "BYN")
-y = Money(11)
-z = Money(12.34, "EUR")
+    def __sub__(self, other):
+        return Money(round(self.value - other.convert_to_def() / Money.exchange_rate[self.currency], 2), self.currency)
 
-print(y*10)
-# print(z + 3.11 * x + y * 0.8)
-# print(f"result in 'USD':{sum([x, y, z])}")
+    __rsub__ = __sub__
+
+    def __truediv__(self, other):
+        return Money(self.value / other, self.currency)
+
+    __rtruediv__ = __truediv__
+
+    def __lt__(self, other):
+        return self.value < other.convert_to_def() / Money.exchange_rate[self.currency]
+
+    def __eq__(self, other):
+        return self.value == other.convert_to_def() / Money.exchange_rate[self.currency]
+
+    def __le__(self, other):
+        return self.value <= other.convert_to_def() / Money.exchange_rate[self.currency]
