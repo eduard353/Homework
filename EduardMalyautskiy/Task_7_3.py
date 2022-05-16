7379459e5fbac82ee41c7a1c11953ca448296757
Eduard Malyautskiy <eduard353@gmail.com>
2022-05-17 01:05:13 +0600

    Add Task 7_3

--- EduardMalyautskiy/Task_7_3.py
+++ EduardMalyautskiy/Task_7_3.py
@@ -1,21 +1,37 @@
 from contextlib import ContextDecorator
+import datetime
+import random
 import time
 
 
 class mycontext(ContextDecorator):
 
+    def __init__(self):
+        self.file = None
+
     def __enter__(self):
         print('Starting')
+        try:
+            self.file = open('exec_time.log', 'a')
+            self.file.write(f'Start time of execute: {datetime.datetime.now()} \n')
+            raise ValueError('adada')
+
+        except Exception as ex:
+            print(f'Somthing was wrong {ex}')
+    
         return self
 
     def __exit__(self, *exc):
+        if self.file is not None:
+            self.file.write(f'Stop time of execute: {datetime.datetime.now()} \n')
         print('Finishing')
-        return False
+        # return False
 
 
 @mycontext()
 def function():
-    pass
+    r = random.randint(2, 4)
+    time.sleep(r)
 
 
 function()
