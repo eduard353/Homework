16eafea81b135c4697e3e2ee25ac58be06ff941b
Edurd M <eduard353@gmail.com>
2022-06-09 17:39:27 +0600

    Add Verbose mode

--- EduardMalyautskiy/rss_parser.py
+++ EduardMalyautskiy/rss_parser.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from bs4 import BeautifulSoup
 import requests
 import logging
@@ -7,8 +8,16 @@ headers = {
     'User-Agent': 'your-user-agent-here'
 }
 
-logging.basicConfig(level=logging.INFO)
-LOG = logging.getLogger(__name__)
+keys_dict = {
+    "title": "Title",
+    "link": "Link",
+    "atom:link": "Link",
+    "pubdate": "Date",
+    "description": "Description",
+    "source": "Source",
+    "media:content": "Media",
+
+}
 
 
 def createParser():
@@ -28,8 +37,9 @@ class ReadRss:
     """
     Класс парсера RSS ленты
     """
-    def __init__(self, rss_url, headers):
-        self.req_tags = ['title', 'pubdate', 'link']
+
+    def __init__(self, rss_url, limit, headers):
+
         self.url = rss_url
         self.headers = headers
         try:
@@ -54,45 +64,50 @@ class ReadRss:
             self.tags.add(tag.name)
         self.articles_dicts = []
 
-        for a in self.articles:
-            print(a)
+        for a in self.articles[:limit]:
+            # print(a)
             item_to_dict = dict()
             for tag in self.tags:
                 try:
-                    text = a.find(tag).text
+                    text = self.formatdata(str(a.find(tag).text))
 
                     if text is not None and text != '':
-                        item_to_dict[tag] = a.find(tag).text
+                        item_to_dict[tag] = text
+                    elif 'href' in a.find(tag).attrs:
+                        item_to_dict[tag] = text + '(' + a.find(tag)['href'] + ')'
+                    elif 'url' in a.find(tag).attrs:
+                        item_to_dict[tag] = text + '(' + a.find(tag)['url'] + ')'
                     elif tag == 'link':
-                        item_to_dict['link'] = a.find(tag).next_sibling.replace('\n', '').replace('\t', '')
-                    elif tag == 'atom:link' and a.find(tag)['href']:
-                        item_to_dict['link'] = a.find(tag)['href']
+                        item_to_dict[tag] = a.find(tag).next_sibling.replace('\n', '').replace('\t', '')
 
-                        # self.dicts.append({t:a.find(t).text for t in self.tags})
                 except AttributeError as e:
                     print(f'Could not find tag "{tag}" in item.')
                     print(e)
             self.articles_dicts.append(item_to_dict)
 
+    @staticmethod
+    def formatdata(data):
+        result = data.replace('\n', '').replace('\t', '').replace('<![CDATA[', '').replace(']]>', '')
+        return result
+
 
 if __name__ == '__main__':
     parser = createParser()
     namespace = parser.parse_args()
+    if namespace.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
-    feed = ReadRss(namespace.source.replace("'", ''), headers)
+    feed = ReadRss(namespace.source.replace("'", ''), namespace.limit, headers)
 
     print('Feed: ', feed.feed, end='\n\n')
 
     for item in feed.articles_dicts:
 
-        for req_tag in feed.req_tags:
-            print(f'{req_tag}: ', item.get(req_tag))
-        for nreq_tag in set(feed.tags).difference(set(feed.req_tags)):
-            print(f'{nreq_tag}: ', item.get(nreq_tag))
-        print('-'*60)
+        for tag in feed.tags:
+            if keys_dict.get(tag) and item.get(tag):
+                print(f'{keys_dict[tag]}: ', item.get(tag))
 
-
-
-    # print(feed.tags)
-    # print(feed.articles_dicts)
-
+        print('-' * 60)
+    print(feed.char_code)
